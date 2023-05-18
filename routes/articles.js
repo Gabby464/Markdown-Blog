d71a8b9a775f9632171c886fc8298cf5d97259ab
Gabriela P <gabriela.poryazova@gmail.com>
2023-05-18 22:34:40 +0300

    Apply 'show details' logic and rendering

--- routes/articles.js
+++ routes/articles.js
@@ -1,23 +1,33 @@
-const express = require('express');
-const Article = require('../models/article')
-const router = express.Router()
+const express = require("express");
+const Article = require("../models/article");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index', {text: 'Hello Articles'})
+router.get("/", async (req, res) => {
+    const articles = await Article.find({});
+    console.log(articles)
+    res.render("articles/index", { articles: articles});
 });
 
-router.get('/new',(req, res) => {
-    res.render('articles/new');
-})
+router.get("/new", (req, res) => {
+  res.render("articles/new", { article: new Article() });
+});
 
-router.post('/', async (req, res) => {
-    console.log(req.body)
-    const article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown,
-    })
-    await article.save();
-    res.redirect(`/aritcles/${article.id}`)
-})
-module.exports = router
+router.get("/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  if (!article) {
+    throw new Error();
+  }
+  console.log("working");
+  res.render("articles/show", { article: article });
+});
+
+router.post("/", async (req, res) => {
+  const article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  });
+  await article.save();
+  res.redirect(`/articles/${article.id}`);
+});
+module.exports = router;
