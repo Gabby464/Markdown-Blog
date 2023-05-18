57dc5c1fe3ca7464d039eaa1b01b7e48d8698b2e
Gabriela P <gabriela.poryazova@gmail.com>
2023-05-18 18:12:58 +0300

    Post Article document to the server

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,23 @@
-const express = require('express')
+const express = require('express');
+const Article = require('../models/article')
 const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('index', {text: 'Hello Articles'})
 });
 
+router.get('/new',(req, res) => {
+    res.render('articles/new');
+})
+
+router.post('/', async (req, res) => {
+    console.log(req.body)
+    const article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown,
+    })
+    await article.save();
+    res.redirect(`/aritcles/${article.id}`)
+})
 module.exports = router
