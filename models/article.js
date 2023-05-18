edf0b35aa04956a74d3fe9ff392938504f73f406
Gabriela P <gabriela.poryazova@gmail.com>
2023-05-18 17:39:31 +0300

    Set up article model

---
+++ models/article.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    required: true, 
+    type: String
+  },
+  description: {
+    required: true, 
+    type: String
+  },
+  markdown: {
+    type: String, 
+    required: true, 
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date().toLocaleDateString(),
+  }
+
+});
+
+module.exports = mongoose.model('Article', articleSchema)
