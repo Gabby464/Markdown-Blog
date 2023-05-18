2fa772dcb68e64a2038f4437ce94ea0fd0986aaa
Gabriela P <gabriela.poryazova@gmail.com>
2023-05-18 17:54:05 +0300

    Install erorr handling package

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
+//async errors
+require('express-async-errors')
 const app = express();
 const articleRouter = require('./routes/articles');
 const connectDB = require('./db/connect')
 require('dotenv').config()
 //set view engine
 app.set('view engine', 'ejs')
-app.use('/articles',articleRouter)
+app.use('/articles',articleRouter);
+//access all params from the article form
+app.use(express.urlencoded({extended: false}))
+
+//error middleware
+
 app.get('/', (req, res) => {
     const articles = [{
         title: 'test',
