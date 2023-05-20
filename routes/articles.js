bfb5c40e0dd616da3cf02c72f7d7eee2afc75f65
Gabriela P <gabriela.poryazova@gmail.com>
2023-05-20 13:10:35 +0300

    Add "edit" article func

--- routes/articles.js
+++ routes/articles.js
@@ -2,36 +2,59 @@ const express = require("express");
 const Article = require("../models/article");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-    const articles = await Article.find({});
-    res.render("articles/index", { articles: articles});
-});
+// router.get("/", async (req, res) => {
+//   const articles = await Article.find({});
+//   res.render("articles/index", { articles: articles });
+// });
 
 router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
 });
 
+router.get("/edit/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render("articles/edit", { article: article });
+});
+
 router.get("/:slug", async (req, res) => {
-  const article = await Article.findOne({slug: req.params.slug});
+  const article = await Article.findOne({ slug: req.params.slug });
   if (!article) {
     throw new Error();
   }
-  console.log("working");
   res.render("articles/show", { article: article });
 });
 
-router.post("/", async (req, res) => {
-  const article = new Article({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown,
-  });
-  await article.save();
-  res.redirect(`/articles/${article.slug}`);
+router.put(
+  "/:id",
+  async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+  },
+  saveArticleandRedirect()
+);
+
+router.post(
+  "/",
+   (req, res, next) => {
+    (req.article =  new Article()), 
+    next();
+  },
+  saveArticleandRedirect("new")
+);
+
+router.delete("/:id", async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.redirect("/");
 });
 
-router.delete('/:id', async(req, res)=> {
-    await Article.findByIdAndDelete(req.params.id);
-    res.redirect('/')
-})
+function saveArticleandRedirect() {
+  return async (req, res) => {
+    const article = req.article;
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
+    await article.save();
+    res.redirect(`/`);
+  };
+}
 module.exports = router;
