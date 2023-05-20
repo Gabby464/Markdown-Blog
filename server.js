f1a5125032f5b36ad3da14a55a6e31a6d4924605
Gabriela P <gabriela.poryazova@gmail.com>
2023-05-20 13:34:43 +0300

    Organize/comment the server file for improved readability

--- server.js
+++ server.js
@@ -1,41 +1,48 @@
 const express = require("express");
-
-//async errors
+const methodOverride = require("method-override");
+const dotenv = require("dotenv");
+const article = require('./models/article')
+// Async errors
 require("express-async-errors");
 
-const app = express();
+// Load environment variables
+dotenv.config();
+
+// Import routes
 const articleRouter = require("./routes/articles");
+
+// Import middlewares
 const connectDB = require("./db/connect");
 const errorHandling = require("./middlewares/error-handling");
 const notFound = require("./middlewares/not-found");
-const renderMiddleware = require("./middlewares/render");
-const article = require("./models/article");
-const methodOverride = require('method-override')
-require("dotenv").config();
 
-//set view engine
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'))
+const app = express();
+
+// Set view engine
 app.set("view engine", "ejs");
-//access all params from the article form
-app.use("/articles", articleRouter);
-//middleware
 
- // delete button will be in a form el and method's gonna be delete
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+// Access all params from the article form
+app.use("/articles", articleRouter);
+
+// Home page route
 app.get("/", async (req, res) => {
-    const articles = await article.find({}).sort({
-        createdAt: "desc"
-    });
-    res.render("articles/index", { articles: articles});
+  const articles = await article.find({}).sort({
+    createdAt: "desc",
+  });
+  res.render("articles/index", { articles: articles });
 });
 
-//error middleware
-app.use(notFound)
+// Error middleware
+app.use(notFound);
 app.use(errorHandling);
-
 
 const start = async () => {
   await connectDB(process.env.MONGO_URI);
-  app.listen(5000, console.log("sever is listening on port 3000"));
+  app.listen(5000, console.log("Server is listening on port 5000"));
 };
+
 start();
